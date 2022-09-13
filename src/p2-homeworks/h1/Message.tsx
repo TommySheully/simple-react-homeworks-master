c46597cd6d1a99f7c98a9ed917c9615e40cb9d01
Tommy <dfgthii@gmail.com>
2022-09-13 22:02:44 +0300

    yarn install сделал дз 1 добавил типы и модульный файл Message.module.css для файла Message.tsx сделал корявую, но работающую верстку для сообщения. Тесты прошел, аж сам не ожидал.

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import s from "./Message.module.css";
 
-function Message() {
+type MessageProps = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string,
+}
+
+function Message(props: MessageProps) {
     return (
-        <div>
-
+        <div className={s.message}>
+            <div className={s.avatar}>
+                <img src={props.avatar} alt='avatar'/>
+            </div>
+            <div className={s.messageBody}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.messageText}>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
+            </div>
         </div>
     )
 }
