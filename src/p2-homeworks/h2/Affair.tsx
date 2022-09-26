b088a2dbbe1883a45652d834fef4f2ec21ffdb09
Tommy <dfgthii@gmail.com>
2022-09-26 13:52:08 +0300

    filters work

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import {AffairType} from "./HW2";
+import affairs from "./Affairs";
+
 
 type AffairPropsType = {
     // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+    affair: AffairType, // need to fix any
+    deleteAffairCallback: (affairId: number) => void, // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+
+    const deleteCallback = () => {
+        props.deleteAffairCallback(props.affair._id)
+        console.log(props.affair._id)
+    }// need to fix
 
     return (
         <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
+            {props.affair.name}
+            <button onClick={() => deleteCallback}>X</button>
         </div>
     )
 }
